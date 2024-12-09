9d7e046459e169947173afc0842dce26fafb7e9c
LehaGrand2018 <alekseygisich@yandex.by>
2024-12-09 16:48:08 +0300

    add question search

--- src/App/Screens/QuestionsList/QuestionsList.jsx
+++ src/App/Screens/QuestionsList/QuestionsList.jsx
@@ -4,13 +4,15 @@ import styles from "./QuestionsList.module.scss";
 import PropTypes from "prop-types";
 import { getQuestions } from "../../Requests/QuestionsRequests";
 import { useLocation } from "react-router-dom";
+import Search from "../../Components/Search/Search";
 
 const QuestionsList = ({ className }) => {
   const [questions, setQuestions] = useState(null);
   const [questionsToDisplay, setQuestionsToDisplay] = useState(null);
 
+  const [searchTerm, setSearchTerm] = useState("");
   const location = useLocation();
-  const params = new URLSearchParams(location.search);
+  const [params] = useState(new URLSearchParams(location.search));
   let paramsObj = {};
   params.forEach((param, key) => {
     if (key === "tags") {
@@ -22,13 +24,14 @@ const QuestionsList = ({ className }) => {
     }
     paramsObj[key] = param;
   });
+
   //load questions
   useEffect(() => {
     (async () => {
       setQuestions((await getQuestions(paramsObj)).questions);
     })();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location.search]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params.toString()]);
 
   // display questions
   useEffect(() => {
@@ -53,9 +56,36 @@ const QuestionsList = ({ className }) => {
     }
   }, [questions]);
 
+  useEffect(() => {
+    if (params.get("content") === "") {
+      params.delete("content");
+    }
+    location.search = params.toString();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params.toString()]);
+
+  useEffect(() => {
+    setContentParam(searchTerm);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchTerm]);
+
+  const setContentParam = (content) => {
+    params.set("content", content);
+  };
+
   return (
     <div className={`${styles.questionsList} ${className}`}>
-      <h2 className={styles.header}>Вопросы</h2>
+      <div className={styles.header}>
+        <h2 className={styles.title}>Вопросы</h2>
+        <Search
+          className={styles.search}
+          type="text"
+          placeholder="Поиск"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          hideButton={"true"}
+        ></Search>
+      </div>
       {questionsToDisplay}
     </div>
   );
